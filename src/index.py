99187ce59275261160d468e8478314bfe0df1a97
Yan Rodrigues <ccyan05@alu.ufc.br>
2021-12-29 11:13:47 -0300

    Adding more test cases

--- src/index.py
+++ src/index.py
@@ -1,9 +1,16 @@
 from Analyzer import Analyzer
 
-formula = "((p>(p#q))&r)"
+checkers = [
+    "((p > q) > r)",
+    "p#q",
+    "(p&q > r # q)",
+    "p > 2",
+    "a > 1 > 3"
+]
 
-check = Analyzer(formula)
-print(check.isFormula())
+for check in checkers:
+    formula = Analyzer(check)
+    print(check, formula.isFormula())
 
 
 
