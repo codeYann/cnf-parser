f885185fedd26e9691b1b2a12db5cd1a0101c17a
Yan Rodrigues <ccyan05@alu.ufc.br>
2022-01-06 15:53:15 -0300

    Implementing push_negations & remove_double_neg

--- src/Fnc.py
+++ src/Fnc.py
@@ -51,3 +51,46 @@ def remove_implies(formula: str) -> str:
         k += 1
 
     return formula
+
+
+def switch_operatores(formula: str, start: int, end: int) -> str:
+    formula = list(formula)
+    for i in range(start, end + 1):
+        if formula[i] == "#":
+            formula[i] = "&"
+        elif formula[i] == "&":
+            formula[i] = "#"
+    return "".join(formula)
+
+
+def push_negations(formula: str) -> str:
+    k = 0
+    while "-(" in formula:
+        if formula[k] == "#" or formula[k] == "&":
+            idx_i, idx_f = get_subformula(formula, k)
+            formula = switch_operatores(formula, idx_i, idx_f)
+
+            if formula[idx_i - 1] == "-":
+                print(formula)
+                formula = (
+                    formula[0 : idx_i - 1]
+                    + formula[idx_i]
+                    + "-"
+                    + formula[idx_i + 1 : k + 1]
+                    + "-"
+                    + formula[k + 1 :]
+                )
+                k = 0
+        k += 1
+
+    return formula
+
+
+def remove_double_neg(formula: str) -> str:
+    while "--" in formula:
+        formula = formula.replace("--", "")
+    return formula
+
+
+print("(((p>q)>p)>p)")
+print(remove_double_neg(push_negations(remove_implies("(((p>q)>p)>p)"))))
