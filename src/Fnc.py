c71165ba6d8e1574599a142ec1c34376834c98de
Yan Rodrigues <ccyan05@alu.ufc.br>
2022-01-06 10:07:55 -0300

    Implementing remove_implications

--- src/Fnc.py
+++ src/Fnc.py
@@ -1,1 +1,34 @@
-print("Hello!")
+import Analyzer as linter
+
+
+def add_brackets(formula: str) -> str:
+    if formula[0] != "(":
+        return "(" + formula + ")"
+    elif formula[0] == "(":
+        if formula[-1] != ")":
+            return "(" + formula + ")"
+    return formula
+
+
+def remove_implications(formula: str):
+    formula = add_brackets(linter.format(formula))
+    print(formula)
+    formula = list(formula)
+
+    fnc = formula
+    i = 0
+
+    while ">" in formula:
+        if formula[i] == ">":
+            if formula[i - 1] in linter.atoms:
+                fnc[i] = "#"
+                fnc.insert(i - 1, "-")
+            else:
+                pass
+        i += 1
+    return fnc
+
+
+formula = "p>q"
+formula = remove_implications(formula)
+print(formula)
